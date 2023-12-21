6b55cd96d8a604f30fc522f9a10a73dd16e625fb
Diana Beki <dianabeki54@gmail.com>
2023-12-21 19:17:22 +0300

    Include all tools that i have used

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import css from '../assets/css.png';
-import github from '../assets/github.png';
-import html from '../assets/html.png';
-import javascript from '../assets/javascript.png';
-import node from '../assets/node.png';
-import reactImg from '../assets/react.png';
-import tailwind from '../assets/tailwind.png';
+import css from '../assets/experience/css.png';
+import github from '../assets/experience/github.png';
+import html from '../assets/experience/html.png';
+import javascript from '../assets/experience/javascript.png';
+import node from '../assets/experience/node.png';
+import reactImg from '../assets/experience/react.png';
+import tailwind from '../assets/experience/tailwind.png';
+import codepen from '../assets/experience/codepen.jpg';
+import vscode from '../assets/experience/vscode.png';
+import typescript from '../assets/experience/typescript.png';
+import postgresql from '../assets/experience/Postgresql.png';
+import git from '../assets/experience/git.png';
 
 const Experience = () => {
   const techs = [
@@ -13,58 +18,88 @@ const Experience = () => {
       id: 1,
       src: html,
       title: 'HTML',
-      style: 'shadow-[#F39F5A]',
+      style: 'shadow-[#D83F31]',
     },
     {
       id: 2,
       src: css,
       title: 'CSS',
-      style: 'shadow-blue-500',
+      style: 'shadow-[#E9B824]',
     },
     {
       id: 3,
       src: javascript,
       title: 'JavaScript',
-      style: 'shadow-yellow-500',
+      style: 'shadow-[#F3FDE8]',
     },
     {
       id: 4,
       src: reactImg,
       title: 'React',
-      style: 'shadow-blue-600',
+      style: 'shadow-[#EC53B0]',
     },
     {
       id: 5,
       src: tailwind,
       title: 'Tailwind',
-      style: 'shadow-sky-400',
+      style: 'shadow-[#9A3B3B]',
     },
     {
       id: 6,
       src: github,
       title: 'Github',
-      style: 'shadow-gray-400',
+      style: 'shadow-[#FFBA86]',
     },
     {
       id: 7,
       src: node,
       title: 'Node',
-      style: 'shadow-lime-800',
+      style: 'shadow-[#A8DF8E]',
+    },
+    {
+      id: 8,
+      src: vscode,
+      title: 'Vscode',
+      style: 'shadow-[#52D3D8]',
+    },
+    {
+      id: 9,
+      src: codepen,
+      title: 'Codepen',
+      style: 'shadow-[#FF8F8F]',
+    },
+    {
+      id: 10,
+      src: typescript,
+      title: 'Typescript',
+      style: 'shadow-[#5FBDFF]',
+    },
+    {
+      id: 11,
+      src: postgresql,
+      title: 'PostgreSQL',
+      style: 'shadow-[#AF2655]',
+    },
+    {
+      id: 12,
+      src: git,
+      title: 'Git',
+      style: 'shadow-[#D2DE32]',
     },
   ];
 
   return (
-    <div name="experience" className="bg-gradient-to-b from-black to-gray-800 pb-[2rem] md:pb-[10rem]">
-      <div className="flex flex-col justify-center text-[#FFF7D4]">
+    <div name="experience" className="bg-tools w-full pb-[2rem] md:pb-[10rem]">
+      <div className="flex flex-col justify-center text-[#186F65]">
         <div className="text-center">
-          <h2 className="bg-clip-text bg-gradient-to-b from-[#B6FFFA] to-[#FF6C22] text-transparent text-3xl md:text-4xl pt-5 md:pt-10">Experience</h2>
+          <h2 className="text-3xl md:text-4xl pt-5 md:pt-10">Tools</h2>
         </div>
         <div className="grid grid-cols-2 md:grid-cols-3 gap-8 text-center py-8 md:px-12 px-5">
           {techs.map(({
             id, src, title, style,
           }) => (
             <div key={id} className={`shadow-md hover:skew-12 hover:scale-y-75 duration-500 py-2 rounded-lg ${style}`}>
-              <img src={src} alt="" className="w-20 mx-auto" />
+              <img src={src} alt="tool" className="w-20 mx-auto" />
               <p className="mt-4">{title}</p>
             </div>
           ))}
